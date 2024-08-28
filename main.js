16ad15441e76b0eef3c70d399abc853a27630835
MinhHoangtnat <nguyenhoang2439@gmail.com>
2024-08-28 12:22:20 +0700

    Add file

---
+++ main.js
@@ -0,0 +1,11 @@
+function run(){
+    var my_number = 24;
+    var my_string = "JavaScipt";
+    var my_array = [1, 2];
+    var my_object = {a: 'x', b: 'y'};
+    console.log(my_number);
+    console.log(my_string);
+    console.log(my_array);
+    console.log(my_object);
+};
+run();
